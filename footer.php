37f61eaeb59d4506aa3d85df5955fc79126ab73c
Tim Spinks <tim@ninthlink.com>
2015-05-07 15:54:02 -0700

    layout updates

---
+++ footer.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package pgb
+ */
+$options = pgb_get_options();
+?>
+				
+		</div><!-- close .row -->
+	</div><!-- close .container -->
+	<?php tha_content_after(); ?>
+</div><!-- close .main-content -->
+
+<!-- Footer widget area -->
+<div class="footerwidgetarea">
+	<div class="container">
+		<div class="row">
+			<?php 
+				$metabox_custom_page_footer 	  = get_post_meta(get_the_ID(), 'metabox_page_footer_option', true);
+				$metabox_custom_page_footer_count = get_post_meta(get_the_ID(), 'custom_footer_layout', true);
+				$showFooter 					  = $options['footer'];
+				$footer_column 					  = $options['footer_column'];
+
+				if ( is_page() ) {
+					if( $metabox_custom_page_footer == "default" ) {
+						if( !empty( $showFooter ) && '1' == $showFooter ) {
+							if ( !empty( $footer_column )) {
+								if ( $footer_column == 'default' ) {
+										?>
+										<div class="col-sm-4">
+										<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-widget') ) : ?>
+										<?php endif; ?>
+										</div>
+										<?php
+									for ( $i = 2 ; $i <= 3; $i++) {
+										?>
+										<div class="col-sm-4">
+										<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( 'footer-widget-'.$i ) ) : ?>
+										<?php endif; ?>
+										</div>
+										<?php
+									}
+								} else {
+									$num = 12/$footer_column;
+										?>
+										<div class="col-sm-<?php echo $num; ?>">
+										<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-widget') ) : ?>
+										<?php endif; ?>
+										</div>
+										<?php
+									for ( $i = 2 ; $i <= $footer_column; $i++) {
+										?>
+										<div class="col-sm-<?php echo $num; ?>">
+										<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( 'footer-widget-'.$i ) ) : ?>
+										<?php endif; ?>
+										</div>
+										<?php
+									}
+								}
+							}
+						}
+					} else if( $metabox_custom_page_footer == "custom" ) {
+						$num = 12/$metabox_custom_page_footer_count;
+							?>
+							<div class="col-sm-<?php echo $num; ?>">
+							<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-widget') ) : ?>
+							<?php endif; ?>
+							</div>
+							<?php
+						for ( $i = 2 ; $i <= $metabox_custom_page_footer_count; $i++) {
+							?>
+							<div class="col-sm-<?php echo $num; ?>">
+							<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( 'footer-widget-'.$i ) ) : ?>
+							<?php endif; ?>
+							</div>
+							<?php
+						}
+					}
+				} else if( !empty( $showFooter ) && '1' == $showFooter ) {
+					if ( !empty($footer_column)) {
+						if ( $footer_column == 'default' ) {
+								?>
+								<div class="col-sm-4">
+								<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-widget') ) : ?>
+								<?php endif; ?>
+								</div>
+								<?php
+							for ( $i = 2 ; $i <= 3; $i++) {
+								?>
+								<div class="col-sm-4">
+								<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( 'footer-widget-'.$i ) ) : ?>
+								<?php endif; ?>
+								</div>
+								<?php
+							}
+			 			} else { 
+							$num = 12/$footer_column;
+								?>
+								<div class="col-sm-<?php echo $num; ?>">
+								<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-widget') ) : ?>
+								<?php endif; ?>
+								</div>
+								<?php
+							for ( $i = 2 ; $i <= $footer_column; $i++) {
+								?>
+								<div class="col-sm-<?php echo $num; ?>">
+								<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( 'footer-widget-'.$i ) ) : ?>
+								<?php endif; ?>
+								</div>
+								<?php
+							}
+			 			} 
+			 		} 
+				}
+			?>
+		</div>
+	</div>
+</div>
+
+<?php tha_footer_before(); ?>
+<footer id="colophon" class="site-footer" role="contentinfo">
+	<?php tha_footer_top(); ?>
+	<div class="container">
+		<div class="row">
+			<div class="site-footer-inner-top col-sm-12">
+				<?php echo pgb_get_mobile_logo(); ?>
+				<?php // Main Menu
+				wp_nav_menu(
+					array(
+						'theme_location' => 'footer',
+						'container' => false,
+						//'container_class' => 'top-view-primary-menu',
+						'menu_class' => 'nav navbar-nav ',
+						'fallback_cb' => '',
+						'menu_id' => 'footer-menu',
+						'walker' => new wp_bootstrap_navwalker()
+					)
+				);
+				?>
+				<?php if ( is_active_sidebar( 'footer_social' ) ) : ?>
+					<div class="pull-right"><?php dynamic_sidebar( 'footer_social' ); ?></div>
+				<?php endif; ?>
+			</div>
+			<div class="site-footer-inner col-sm-12">
+
+				<div class="site-info">
+					<?php do_action( 'pgb_credits' ); ?>
+				</div><!-- close .site-info -->
+
+			</div>
+		</div>
+	</div><!-- close .container -->
+	<?php tha_footer_bottom(); ?>
+</footer><!-- close #colophon -->
+<?php tha_footer_after(); ?>
+<?php tha_body_bottom(); ?>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package pgb
+ */
+?><!DOCTYPE html>
+<?php tha_html_before(); ?>
+<html <?php language_attributes(); ?>>
+<head>
+
+    <?php tha_head_top(); ?>
+
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+    <?php tha_head_bottom(); ?>
+
+    <?php $pagecolor = inspirations_page_color(); ?>
+
+    <style type="text/css">
+    .page-color {
+      background-color: <?php echo $pagecolor; ?>;
+    }
+    </style>
+
+    <?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+
+    <?php tha_body_top(); ?>
+    <?php tha_header_before(); ?>
+    <?php do_action( 'before' ); ?>
+
+    <?php pgb_block_navtop(); ?>
+
+    <div id="page-content-wrapper" class="page-content-wrapper-left">
+
+      <?php pgb_block_header(); //locate_template('block-header.php', true); ?>
+
+      <?php tha_header_after();  ?>
+      <?php tha_content_before(); ?>
+      <div class="main-content">
+        <div class="container">
+          <div class="row">
