93ade69b26e984766f9b60505799adcd0f23087d
Tim Spinks <tim@ninthlink.com>
2015-08-18 15:14:53 -0700

    disabled comments and posts nav

--- single.php
+++ single.php
@@ -43,12 +43,11 @@ get_header(); ?>
 
 			<?php tha_entry_after(); ?>
 
-			<?php pgb_content_nav( 'nav-below' ); ?>
+			<?php //pgb_content_nav( 'nav-below' ); ?>
 
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || '0' != get_comments_number() )
-					comments_template();
+				//if ( comments_open() || '0' != get_comments_number() ) comments_template();
 			?>
 
 		<?php endwhile; // end of the loop. ?>
