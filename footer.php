ab8adb751d98abb4ced1e5e3b617d92281536d5c
Tim Spinks <tim@ninthlink.com>
2015-08-27 13:09:45 -0700

    footerfix

--- footer.php
+++ footer.php
@@ -18,104 +18,10 @@ $options = pgb_get_options();
 <div class="footerwidgetarea">
 	<div class="container">
 		<div class="row">
-			<?php 
-				$metabox_custom_page_footer 	  = get_post_meta(get_the_ID(), 'metabox_page_footer_option', true);
-				$metabox_custom_page_footer_count = get_post_meta(get_the_ID(), 'custom_footer_layout', true);
-				$showFooter 					  = ( isset($options['footer']) ? $options['footer'] : '' );
-				$footer_column 					  = ( isset($options['footer_column']) ? $options['footer_column'] : '' );
-
-				if ( is_page() ) {
-					if( $metabox_custom_page_footer == "default" ) {
-						if( !empty( $showFooter ) && '1' == $showFooter ) {
-							if ( !empty( $footer_column )) {
-								if ( $footer_column == 'default' ) {
-										?>
-										<div class="col-sm-4">
-										<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-widget') ) : ?>
-										<?php endif; ?>
-										</div>
-										<?php
-									for ( $i = 2 ; $i <= 3; $i++) {
-										?>
-										<div class="col-sm-4">
-										<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( 'footer-widget-'.$i ) ) : ?>
-										<?php endif; ?>
-										</div>
-										<?php
-									}
-								} else {
-									$num = 12/$footer_column;
-										?>
-										<div class="col-sm-<?php echo $num; ?>">
-										<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-widget') ) : ?>
-										<?php endif; ?>
-										</div>
-										<?php
-									for ( $i = 2 ; $i <= $footer_column; $i++) {
-										?>
-										<div class="col-sm-<?php echo $num; ?>">
-										<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( 'footer-widget-'.$i ) ) : ?>
-										<?php endif; ?>
-										</div>
-										<?php
-									}
-								}
-							}
-						}
-					} else if( $metabox_custom_page_footer == "custom" ) {
-						$num = 12/$metabox_custom_page_footer_count;
-							?>
-							<div class="col-sm-<?php echo $num; ?>">
-							<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-widget') ) : ?>
-							<?php endif; ?>
-							</div>
-							<?php
-						for ( $i = 2 ; $i <= $metabox_custom_page_footer_count; $i++) {
-							?>
-							<div class="col-sm-<?php echo $num; ?>">
-							<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( 'footer-widget-'.$i ) ) : ?>
-							<?php endif; ?>
-							</div>
-							<?php
-						}
-					}
-				} else if( !empty( $showFooter ) && '1' == $showFooter ) {
-					if ( !empty($footer_column)) {
-						if ( $footer_column == 'default' ) {
-								?>
-								<div class="col-sm-4">
-								<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-widget') ) : ?>
-								<?php endif; ?>
-								</div>
-								<?php
-							for ( $i = 2 ; $i <= 3; $i++) {
-								?>
-								<div class="col-sm-4">
-								<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( 'footer-widget-'.$i ) ) : ?>
-								<?php endif; ?>
-								</div>
-								<?php
-							}
-			 			} else { 
-							$num = 12/$footer_column;
-								?>
-								<div class="col-sm-<?php echo $num; ?>">
-								<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-widget') ) : ?>
-								<?php endif; ?>
-								</div>
-								<?php
-							for ( $i = 2 ; $i <= $footer_column; $i++) {
-								?>
-								<div class="col-sm-<?php echo $num; ?>">
-								<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar( 'footer-widget-'.$i ) ) : ?>
-								<?php endif; ?>
-								</div>
-								<?php
-							}
-			 			} 
-			 		} 
-				}
-			?>
+			<div class="col-sm-4">
+			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-widget') ) : ?>
+			<?php endif; ?>
+			</div>
 		</div>
 	</div>
 </div>
